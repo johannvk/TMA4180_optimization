de1f25e4492581acaae153812417afc1cb59e854
johannvk <johannesvk@protonmail.com>
2020-02-06 18:11:40 +0100

    First visualization function for robot arm made

--- visualization.py
+++ visualization.py
@@ -2,7 +2,51 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.lines as lines
 
 
-def get_robot_line_segments(line_lengths, angles):
-    pass
+def get_robot_line_segments(line_lengths, turn_angles):
+    assert(len(line_lengths) == len(turn_angles))
+
+    x_coordinates = [0.0]
+    y_coordinates = [0.0]
+
+    current_angle = 0.0
+
+    for i, l in enumerate(line_lengths):
+        current_angle += turn_angles[i]
+        x_coordinates.append(x_coordinates[i] + l * np.cos(current_angle))
+        y_coordinates.append(y_coordinates[i] + l * np.sin(current_angle))
+
+    return x_coordinates, y_coordinates
+
+
+def display_robot_arm(line_lengths, turn_angles, display=True):
+    x_coordinates, y_coordinates = get_robot_line_segments(line_lengths, turn_angles)
+
+    line_x, line_y = get_robot_line_segments(line_lengths, turn_angles)
+
+    robot_arm = lines.Line2D(line_x, line_y,
+                             c='green', alpha=0.8, linestyle='-', linewidth=2, marker='.', markersize=10, markerfacecolor='red')
+    fig, axis = plt.subplots()
+    axis.add_line(robot_arm)
+
+    fig.suptitle("Robot Arm", fontsize=18)
+    plt.tight_layout()
+
+    axis.autoscale()
+    axis.set(aspect=1)
+
+    # fig.lines.extend([l1])
+    if display:
+        plt.show()
+
+
+def test_display_robot_arm():
+    line_length = [1.0, 2.0, 3.0, 4.0, 5.0]
+    angles = [np.pi/3, -np.pi/3, -np.pi/3, np.pi/3, -np.pi/3]
+
+    display_robot_arm(line_length, angles)
+
+
+test_display_robot_arm()
