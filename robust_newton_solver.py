e28a084bd16048af200e58057eac4e09584f52a9
johannvk <johannesvk@protonmail.com>
2020-02-28 12:42:49 +0100

    Changes to the more robust minimizer, checking and handling non-feasible points, by reverting to simpler method without second-order minimzing steps.

--- robust_newton_solver.py
+++ robust_newton_solver.py
@@ -9,15 +9,15 @@ import visualization
 def find_step_direction(P, f, df, ddf, xk, l):
     try:  # Try to find the optimal Newton direction.
         p = np.linalg.solve(ddf(P, xk, l), -df(P, xk, l))
-        print("\n\nFound Newton step.")
+        # print("\n\nFound Newton step.")
         if p@df(P, xk, l) > 0:
-            print("Newton not decreasing, using direct descent")
+            # print("Newton not decreasing, using direct descent")
             p = -df(P, xk, l)
     except la.linalg.LinAlgError:  # If the Hessian is singular, perform a gradient descent step.
-        print("\n\nNewton not working, taking gradient descent step.\n")
+        # print("\n\nNewton not working, taking gradient descent step.\n")
         p = -df(P, xk, l)
 
-    print("descent:  ", p@df(P, xk, l))
+    # print("descent:  ", p@df(P, xk, l))
     p = p/la.norm(p, 2)
     return p
 
@@ -34,7 +34,7 @@ def line_search_step(P, f, df, ddf, theta_k, line_lengths, c1=0.01, c2=0.8, max_
     # Finding step direction through Newton-method, or reverting back to Gradient descent:
     p = find_step_direction(P, f, df, ddf, theta_k, line_lengths)
 
-    print(f"The step direction is: {p}\n")
+    # print(f"The step direction is: {p}\n")
 
     # initial step length
     a_max = 1e100
@@ -42,36 +42,20 @@ def line_search_step(P, f, df, ddf, theta_k, line_lengths, c1=0.01, c2=0.8, max_
     a = 1
     j = 0
 
-    # Plot first wolfe tests
-    # A = []
-    # F = []
-    # b = a
-    # plt.plot(np.array([0,b]),np.array([f(P,theta_k,l),f(P,theta_k,l)+b*df(P,theta_k,l)@p]), label=r'derivative at $\theta^{(k)}$')
-    # plt.plot(np.array([0,b]),np.array([f(P,theta_k,l),f(P,theta_k,l)+c1*b*df(P,theta_k,l)@p]), label="First wolfe condition")
-
     # Finding an acceptable step length 'a' for the Wolfe conditions:
     while j < max_iter:
-        # For wolfe plotting
-        # A.append(a)
-        # F.append(f(P, theta_k + a * p, l))
-
-        # print(f'\nj={j}')
-        # print(f'a={a}')
-        # print(f'possible improvement={f(P, theta_k, l) - f(P, theta_k + a * p, l)}')
-        # print(f'First wolfe requirement: {f(P, theta_k + a * p, l)}, <= {f(P, theta_k, l)} + {a * c1 * df(P, theta_k,l) @ p}')
-
-        # visualization.display_robot_arm(line_lengths=l, turn_angles=theta_k, target_point=P)
-
         j += 1
+
         f_at_step = f(P, theta_k + a * p, line_lengths)
         f_descent = f(P, theta_k, line_lengths) + a * c1 * df(P, theta_k, line_lengths) @ p
 
         directional_derivative_at_step = df(P, theta_k + a * p, line_lengths).T @ p
         directional_derivative_reduction = c2 * df(P, theta_k, line_lengths).T @ p
-        if f_at_step > f_descent: # Meaning first wolfe-requirement not passed
-            print("First wolfe=Not passed")
+
+        if f_at_step > f_descent:  # Meaning first wolfe-requirement was not passed
             a_max = a
-            a = a_max / 2 + a_min / 2
+            a = 1 * a_max / 4 + 3 * a_min / 4
+
         elif directional_derivative_at_step <= directional_derivative_reduction:
             a_min = a
             if a_max > 1e99:
@@ -83,14 +67,20 @@ def line_search_step(P, f, df, ddf, theta_k, line_lengths, c1=0.01, c2=0.8, max_
     return p, a
 
 
-def second_order_step(H):
+def second_order_step(P, f, ddf, thetas, line_lengths):
     """
     Function finding a descent direction for the Hessian of a function, through eigenvalue decomposition.
     :param H: Hessian of a function.
     :return: (Hopefully) Unit-length eigenvector of the Hessian, with the lowest value eigenvalue.
     """
+    H = ddf(P, thetas, line_lengths)
     w, v = la.eigh(H, 'L')
-    return np.array(v[:, 0])
+    p, a = np.array(v[:, 0]), 1.0
+    # With a short enough step size, we are guaranteed to reduce the function value when we are at a saddle point
+    # and go in a direction p with a negative eigenvalue with respect to the hessian H.
+    while f(P, thetas + a * p, line_lengths) >= f(P, thetas, line_lengths):
+        a *= 0.5
+    return p, a
 
 
 def robust_minimizer(P, f, df, ddf, theta_0, line_lengths, vtol=1e-6, gtol=1.0e-6, max_iter=50, c1=0.01, c2=0.8):
@@ -112,21 +102,32 @@ def robust_minimizer(P, f, df, ddf, theta_0, line_lengths, vtol=1e-6, gtol=1.0e-
     anulus_inner, anulus_outer = anulus_radii(line_lengths)
     target_feasible = anulus_inner <= la.norm(P, 2) < anulus_outer
 
-    # We know that f is non-negative, bounded from below by zero: Assuming that
-    while i < max_iter:
-        i += 1
+    # We know that f is non-negative, bounded from below by zero: Assuming that our target point P
+    # is in the feasible domain, we should be able to reach it with the robot arms.
+    if target_feasible:
+        while i < max_iter:
+            i += 1
 
-        # Check if we have a feasible point, and if close enough, return.
-        if target_feasible and f(P, theta_k, l) < vtol:
-            break
-        elif target_feasible and f(P, theta_k, l) > vtol and la.norm(df(P, theta_k, l)) < gtol:
-            # We are at a saddle point:
-            print("Finding second order step direction!")
-            p = second_order_step(ddf(P, theta_k, l))
-            theta_k += 1.0 * p
-            continue
-        else:
-            # If none of the above apply, perform regular step:
+            # Check if we have a feasible point, and if close enough, return.
+            if f(P, theta_k, l) < vtol:
+                break
+            elif f(P, theta_k, l) > vtol and la.norm(df(P, theta_k, l)) < gtol:
+                # We are at a saddle point:
+                p, a = second_order_step(P, f, ddf, theta_k, l)
+                theta_k += a * p
+                continue
+            else:
+                # If none of the above apply, perform regular step:
+                p, a = line_search_step(P, f, df, ddf, theta_k, line_lengths)
+                theta_k += a * p
+    else:
+        while i < max_iter:
+            i += 1
+
+            # Target point not feasible, cannot easily detect saddle points, and revert to simpler method.
+            # Stopping criterion based on the norm of the gradient.
+            if la.norm(df(P, theta_k, l), 2) < gtol:
+                break
             p, a = line_search_step(P, f, df, ddf, theta_k, line_lengths)
             theta_k += a * p
 
@@ -153,10 +154,10 @@ if __name__ == "__main__":
 
     print("\nproblem 3")
     n = 4
-    theta_0 = np.ones(n) * (0.0 / 2)
-    l = np.array([3, 2, 1, 1])
-    p = (3, 2)
-    theta, f_min = robust_minimizer(p, f, df, ddf, theta_0, line_lengths=l)
+    theta_3 = np.ones(n) * (0.0 / 2)
+    l3 = np.array([3, 2, 1, 1])
+    p3 = (3, 2)
+    theta, f_min = robust_minimizer(p3, f, df, ddf, theta_3, line_lengths=l3)
     print("theta, f(theta)=", theta, f_min)
-    visualization.display_robot_arm(l, theta, p)
+    visualization.display_robot_arm(l3, theta, p3)
 
